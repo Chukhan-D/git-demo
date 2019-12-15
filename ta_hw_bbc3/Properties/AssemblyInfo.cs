da014ea908bf09dc05a483c0b9fdfac4493f2ddd
Dariya Chukhan <chukhan.d@gmail.com>
2019-12-15 13:22:33 +0200

    my

---
+++ ta_hw_bbc3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TA_HW_1(v2)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TA_HW_1(v2)")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95680b7e-7db8-42c5-af89-2033cadb44d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ta_hw_bbc3/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TA_HW_1_v2_
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.bbc.com");
+            driver.FindElement(By.XPath("//div[@id='orb-nav-links']//li[@class='orb-nav-newsdotcom']/a")).Click();
+
+            // IWebElement headline_news = driver.FindElement(By.XPath
+            //     ("//div[@id='news-top-stories-container']//div[1]/div/a[@href='/news/world-asia-china-50541627']/h3"));
+            System.Threading.Thread.Sleep(5000);
+            IWebElement headline_news = driver.FindElement(By.XPath("//div[contains(@class,'gel-layout__item nw-c-top-stories__primary')]//div[contains (@class, 'gs-c-promo-body gs-u-display-none gs-u-display-inline-block@m gs-u-mt@xs')]//div/a[contains(@class,'gs-c-promo-heading')]/h3[contains(@class,'gs-c-promo-heading__title')]"));
+            NUnit.Framework.Assert.AreEqual("Greenhouse gas concentrations break records", headline_news);
+
+            driver.Close();
+        }
+    }
+}
